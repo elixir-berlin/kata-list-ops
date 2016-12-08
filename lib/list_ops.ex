f3d4d6941519bcc9d64a7eb3d037069bd3b1d67b
David Devlin <d.devlin@keelvar.com>
2016-12-08 21:27:31 +0100

    Added reverse/1

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -3,4 +3,9 @@ defmodule ListOps do
 
     defp count([], acc), do: acc
     defp count([h|t], acc), do: count(t, acc+1)
+
+    def reverse(l), do: reverse(l, [])
+
+    defp reverse([], acc), do: acc
+    defp reverse([h|t], acc), do: reverse(t, [h] ++ acc)
 end
