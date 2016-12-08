6732a1a445d5fc91488e347f3423ce6cf845771c
David Devlin <d.devlin@keelvar.com>
2016-12-08 21:48:27 +0100

    Added map/2

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -7,5 +7,10 @@ defmodule ListOps do
     def reverse(l), do: reverse(l, [])
 
     defp reverse([], acc), do: acc
-    defp reverse([h|t], acc), do: reverse(t, [h] ++ acc)
+    defp reverse([h|t], acc), do: reverse(t, [h|acc])
+
+    def map(l, f), do: map(l, f, [])
+
+    defp map([], _, acc), do: reverse(acc)
+    defp map([h|t], f, acc), do: map(t, f, [f.(h)|acc])
 end
