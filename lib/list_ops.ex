e2b65990f8d38fd60a072e7a76cba09c96518ecf
Hugo Duksis <duksis@gmail.com>
2016-12-08 19:37:58 +0100

    Adds count and reverse

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -1,2 +1,21 @@
 defmodule ListOps do
+  def count([]) do
+    0
+  end
+
+  def count([_head | tail]) do
+    1 + count(tail)
+  end
+
+  def reverse(list) do
+    reverse(list, [])
+  end
+
+  def reverse([], acumulator) do
+    acumulator
+  end
+
+  def reverse([head | tail], acumulator) do
+    reverse(tail, [head | acumulator])
+  end
 end
