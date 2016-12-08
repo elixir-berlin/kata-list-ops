244016d5145e6cc52777369d58c6bea8cd14dfd3
Artūrs Mekšs <arturs.mekss@gmail.com>
2016-12-08 21:21:35 +0100

    solution

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -1,2 +1,21 @@
 defmodule ListOps do
+  def count([]) do
+    0
+  end
+
+  def count([head | tail]) do
+    1 + count(tail)
+  end
+
+  def reverse(list) do
+    reverse(list, [])
+  end
+
+  defp reverse([], reverse_list) do
+    reverse_list
+  end
+
+  defp reverse([head | tail], reverse_list) do
+    reverse(tail, [head | reverse_list])
+  end
 end
