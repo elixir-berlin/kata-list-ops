056738188a4437b31b1a8dd77b3fbcde626c7f9e
David Devlin <d.devlin@keelvar.com>
2016-12-09 21:21:11 +0100

    Rewrite count/1, reverse/1, map/2 and filter/2 in terms of reduce/3

--- lib/list_ops.ex
+++ lib/list_ops.ex
@@ -1,29 +1,11 @@
 defmodule ListOps do
-    def count(l), do: count(l, 0)
+    def count(l), do: reduce(l, 0, fn(_, acc) -> acc + 1 end)
 
-    defp count([], acc), do: acc
-    defp count([_|t], acc), do: count(t, acc+1)
+    def reverse(l), do: reduce(l, [], &([&1|&2]))
 
-    def reverse(l), do: reverse(l, [])
+    def map(l, f), do: reduce(l, [], &([f.(&1)|&2])) |> reverse
 
-    defp reverse([], acc), do: acc
-    defp reverse([h|t], acc), do: reverse(t, [h|acc])
-
-    def map(l, f), do: map(l, f, [])
-
-    defp map([], _, acc), do: reverse(acc)
-    defp map([h|t], f, acc), do: map(t, f, [f.(h)|acc])
-
-    def filter(l, f), do: filter(l, f, [])
-
-    defp filter([], _, acc), do: reverse(acc)
-    defp filter([h|t], f, acc) do
-        if f.(h) do
-            filter(t, f, [h|acc])
-        else
-            filter(t, f, acc)
-        end
-    end
+    def filter(l, f), do: reduce(l, [], &(if f.(&1) do [&1|&2] else &2 end)) |> reverse
 
     def reduce([], acc, _), do: acc
     def reduce([h|t], acc, f), do: reduce(t, f.(h, acc), f)
